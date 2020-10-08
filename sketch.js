b7b19493b907bd39cd341ae4811f46a87361d8c0
VampGaming14 <65399588+VampGaming14@users.noreply.github.com>
2020-10-08 19:00:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var batman;
+var maxDrops = 100;
+function preload(){
+    
+}
+
+function setup(){
+ 
+   batman = createSprite(200,200,30,30);
+   umbrella = new Umbrella(300,3430,5,5); 
+}
+
+function draw(){
+    for(var i=0; i<maxDrops; i++){
+     //  drop.push(max i=0; iw
+    }
+
+    
+
+}   
+
---
+++ umbrella.js
@@ -0,0 +1,13 @@
+class Umbrella {
+    constructor(x,y){
+       var engine={
+           friction:0.5,
+           restitution:1
+       }
+    }
+
+
+display(){
+    
+}
+};
